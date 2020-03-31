919526b10a816d42b8c172dd6ec6c23e47bc32c2
PandaTera <47454997+PandaTera@users.noreply.github.com>
2020-03-31 21:20:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('btn');
+const clear = document.getElementById('clear');
+let number = 1;
+const addElement = function (){
+  
+  const div = document.createElement('div')
+ div.textContent = number;
+  document.body.appendChild(div);
+  number++;
+  if(number % 5 == 1){
+      div.className = "box"
+  } else{
+    div.className ="circle"
+  }
+  const clearElement = function (){
+    document.body.removeChild(div);
+}
+
+clear.addEventListener('click', clearElement)
+}
+
+btn.addEventListener('click', addElement);
